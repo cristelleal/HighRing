c081748010ab2d1cae5ad276405bae5c7ca77b88
cristelleal <cristelleal@gmail.com>
2023-05-10 11:06:40 +0200

    add slider at construction brand page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fonctionnement du menu hamburger (mobile device)
+// MENU HAMBURGER 
 
 function toggleMenu () {  
     const navbar = document.querySelector('.navbar');
@@ -18,3 +18,53 @@ function toggleMenu () {
   }
 toggleMenu();
 
+// SLIDER CONSTRUCTION PAGE
+
+// Attendre que le DOM soit chargé
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Sélectionner les éléments du slider
+  var slider = document.querySelector(".slider");
+  var beforeBtn = slider.querySelector(".before");
+  var afterBtn = slider.querySelector(".after");
+  var slideContents = slider.querySelectorAll(".slide-content");
+
+  var currentIndex = 0; // Index du contenu actuel
+
+  // Fonction pour afficher le contenu du slider à l'index spécifié
+  function showSlide(index) {
+    // Masquer tous les contenus
+    slideContents.forEach(function(content) {
+      content.style.display = "none";
+    });
+
+    // Afficher le contenu à l'index spécifié
+    slideContents[index].style.display = "block";
+  }
+
+  // Afficher le premier contenu au chargement de la page
+  showSlide(currentIndex);
+
+  // Gérer le clic sur le bouton précédent
+  beforeBtn.addEventListener("click", function() {
+    currentIndex--; // Décrémenter l'index
+
+    // Vérifier les limites de l'index
+    if (currentIndex < 0) {
+      currentIndex = slideContents.length - 1;
+    }
+
+    showSlide(currentIndex); // Afficher le contenu correspondant
+  });
+
+  // Gérer le clic sur le bouton suivant
+  afterBtn.addEventListener("click", function() {
+    currentIndex++; // Incrémenter l'index
+
+    // Vérifier les limites de l'index
+    if (currentIndex >= slideContents.length) {
+      currentIndex = 0;
+    }
+
+    showSlide(currentIndex); // Afficher le contenu correspondant
+  });
+});
