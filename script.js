8880e96290decbbc39cdda1311ee4e36e1a4487b
cristelleal <cristelleal@gmail.com>
2023-05-19 15:32:09 +0200

    (style) update navbar issue, card issue and navbar links issues

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.onload = () => {
+
 // MENU HAMBURGER 
 
 function toggleMenu () {  
@@ -15,75 +17,77 @@ function toggleMenu () {
       }); 
     })
     
-  }
-  toggleMenu();
-  
-  // SLIDER @CONSTRUCTION PAGE
-  
-  window.onload = () => {
-
-  const slider = document.querySelector('.slider');
-  const beforeBtn = slider.querySelector('.before');
-  const afterBtn = slider.querySelector('.after');
-  const slideContents = slider.querySelectorAll('.slide-content');
-
-  let currentIndex = 0; 
-
-  function showSlide(index) {
-   
-    slideContents.forEach(function(content) {
-      content.style.display = 'none';
-    });
-
-    slideContents[index].style.display = 'block';
-  }
-
-  showSlide(currentIndex);
-
-  beforeBtn.addEventListener('click', function() {
-    currentIndex--; 
-
-    if (currentIndex < 0) {
-      currentIndex = slideContents.length - 1;
-    }
-
-    showSlide(currentIndex); 
-  });
-
-  afterBtn.addEventListener('click', function() {
-    currentIndex++; 
-    
-    if (currentIndex >= slideContents.length) {
-      currentIndex = 0;
-    }
-
-    showSlide(currentIndex); 
-  });
 }
+toggleMenu();
+  
+// SLIDER @CONSTRUCTION PAGE
+  
+  // const slider = document.getElementByClassName('.slider');
+  // const beforeBtn = slider.getElementByClassName('.before');
+  // const afterBtn = slider.getElementByClassName('.after');
+  // const slideContents = slider.getElementsByClassName('.slide-content');
+
+  // let currentIndex = 0; 
+
+  // function showSlide(index) {
+   
+  //   slideContents.forEach(function(content) {
+  //     content.style.display = 'none';
+  //   });
+
+  //   slideContents[index].style.display = 'block';
+  // }
+
+  // showSlide(currentIndex);
+
+  // beforeBtn.addEventListener('click', function() {
+  //   currentIndex--; 
+
+  //   if (currentIndex < 0) {
+  //     currentIndex = slideContents.length - 1;
+  //   }
+
+  //   showSlide(currentIndex); 
+  // });
+
+  // afterBtn.addEventListener('click', function() {
+  //   currentIndex++; 
+    
+  //   if (currentIndex >= slideContents.length) {
+  //     currentIndex = 0;
+  //   }
+
+  //   showSlide(currentIndex); 
+  // });
 
 // PROGRESS BAR
 
-  window.addEventListener('scroll', () => {
+  // window.addEventListener('scroll', () => {
 
-    let height = document.documentElement.scrollHeight - window.innerHeight;
-    let position = window.scrollY;
-    let width = document.documentElement.clientWidth;
+  //   let height = document.documentElement.scrollHeight - window.innerHeight;
+  //   let position = window.scrollY;
+  //   let width = document.documentElement.clientWidth;
 
-    let progress = position / height * width;
+  //   let progress = position / height * width;
 
-    document.getElementById('progress').style.width = progress + 'px';
-  })
+  //   document.getElementById('progress').style.width = progress + 'px';
+  // })
 
 // NAVBAR SCROLL TRANSFORMATION
 
 const nav = document.querySelector('.ui-section-header');
+const navStop = document.querySelector('.ui-section-hero')
 let heightSection = nav.clientHeight;
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > heightSection){
+  if (window.scrollY > heightSection ){
     nav.classList.add('scroll');  
   } else {
     nav.classList.remove('scroll');
   }
 })
+
+}
+
+
 
