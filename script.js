102f44e5f3ae832e148398d142db1959c03e01e7
cristelleal <cristelleal@gmail.com>
2023-05-20 09:36:36 +0200

    error

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ toggleMenu();
 // NAVBAR SCROLL TRANSFORMATION
 
 const nav = document.querySelector('.ui-section-header');
+const navStop = document.querySelector('.ui-section-hero')
 let heightSection = nav.clientHeight;
 
 window.addEventListener('scroll', () => {
