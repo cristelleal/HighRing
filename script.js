1159460e2a58ba7ba885d7dc5ac2289136fd5d16
cristelleal <cristelleal@gmail.com>
2023-06-12 12:11:56 +0200

    erase test

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function toggleMenu() {
   const navbar = document.querySelector('.navbar');
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.ui-section-header');
-  const navbarLinks = document.querySelector('.navbar__links');
 
   burger.addEventListener('click', (e) => {
     e.preventDefault(); 
@@ -23,19 +22,7 @@ function toggleMenu() {
       document.body.style.overflow = ''; 
     }
   });
-
-  // Ajouter une gestion de défilement du menu
-  window.addEventListener('resize', () => {
-    if (window.innerHeight < navbarLinks) {
-      navbarLinks.style.overflowY = 'scroll';
-    } else {
-      navbarLinks.style.overflowY = 'visible';
-    }
-  });
 }
-
-toggleMenu();
-
 
 toggleMenu();
 
